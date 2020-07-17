2afd4fc9b22b8d6a4e42c26a858045abec060397
cojenco <icathyouyang@gmail.com>
2020-07-16 23:13:39 -0700

    Updates

--- precache-manifest.9d2e8bab103120edcc457fb3b5c3e91a.js
+++ precache-manifest.9d2e8bab103120edcc457fb3b5c3e91a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9142ac7174ebc71801d7d2dcbedee482",
+    "revision": "20487e84da63e119eff4893439db2a04",
     "url": "/trailmix-live/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/trailmix-live/static/css/2.829c9cb5.chunk.css"
   },
   {
-    "revision": "653ff0006cb555a94ea0",
+    "revision": "2d388bda91feacb65997",
     "url": "/trailmix-live/static/css/main.91676469.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/trailmix-live/static/js/2.89c2c474.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "653ff0006cb555a94ea0",
-    "url": "/trailmix-live/static/js/main.6e44ad52.chunk.js"
+    "revision": "2d388bda91feacb65997",
+    "url": "/trailmix-live/static/js/main.37891b67.chunk.js"
   },
   {
     "revision": "65c39287adff8051525c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trailmix-live/precache-manifest.806724acbaec6115296665eb1f803898.js"
+  "/trailmix-live/precache-manifest.9d2e8bab103120edcc457fb3b5c3e91a.js"
 );
 
 self.addEventListener('message', (event) => {
