2867218a0ba30252e1f9efb19dc9b944d6bd45a1
cojenco <icathyouyang@gmail.com>
2020-07-19 14:12:19 -0700

    revise styling for home third section

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,9 +16,6 @@ const Home = () => {
         <blockquote className="blockquote text-center">
           <p className="mb-0"> Navigation. Sun Protection. Insulation. Illumination. First-Aid Supplies. Fire. </p>
           <p className="mb-0"> Repair Kit and Tools. Nutrition. Hydration. Emergency Shelter.  </p>
-          {/* <p className="mb-0"> Navigation. Sun Protection. Insulation.  </p>
-          <p className="mb-0"> Illumination. First-Aid Supplies. Fire. Repair Kit and Tools. </p>
-          <p className="mb-0"> Nutrition. Hydration. Emergency Shelter. </p> */}
           <p className="mb-0 text-muted"> (Also, check out Trail Mix Live!) </p>
           <footer class="blockquote-footer">  <a href="https://www.nps.gov/articles/10essentials.htm" className="text-dark" > <cite title="Source Title">National Park Service</cite> </a> </footer>
         </blockquote>
@@ -34,33 +31,32 @@ const Home = () => {
 
        
       <div className="d-flex flex-column justify-content-around align-items-center flex-wrap text-center third-block">
-        {/* <h2 className="hashtag-line" > #RecreateResponsibly </h2> */}
         <blockquote className="blockquote text-center">
-          {/* <p>Remember to recreate responsibly. Your actions can have an impact on park resources and wildlife. </p>
-          <p>WTA reminds us that, "We all have a role to play in keeping people, places and communities safe as we enjoy the outdoors this summer and beyond."</p> */}
           <a href="https://www.wta.org/news/signpost/building-trail-smart-hikers-in-2020" className="align-self-center" >           
           <img
             alt="Recreate Responsibly"
             src="https://www.wta.org/45f42b31-0280-49dc-bd35-2ff052bc85cb.jpg/@@images/4f15d932-f528-4ae8-8e9a-3065713383e5.jpeg"
-            // width="30"
-            // height="30"
             className="d-inline-block align-top w-50"
           /> 
           </a>
-          <footer class="blockquote-footer">  <a href="https://www.wta.org/go-outside/trail-smarts" className="text-light" > <cite title="Source Title">Washington Trails Association, Washington’s Recreate Responsibly Coalition</cite> </a> </footer>
+          {/* <footer class="blockquote-footer">  <a href="https://www.wta.org/go-outside/trail-smarts" className="text-light" > <cite title="Source Title">Washington Trails Association, Washington’s Recreate Responsibly Coalition</cite> </a> </footer> */}
         </blockquote>
         <h2 className="hashtag-line" > #RecreateResponsibly </h2>
 
-        <article>
+        {/* <article>
           <p>Remember to recreate responsibly. Your actions can have an impact on trails and wildlife. </p>
           <p>WTA/RRC reminds us that, "We all have a role to play in keeping people, places and communities safe as we enjoy the outdoors this summer and beyond."</p>
-        </article>
-      </div>
+        </article> */}
 
+        <blockquote className="blockquote text-center">
+          <p>Your actions can have an impact on trails and wildlife. </p>
+          <p>"We all have a role to play in keeping people, places and communities safe as we enjoy the outdoors this summer and beyond."</p>
+          <footer class="blockquote-footer">  <a href="https://www.wta.org/go-outside/trail-smarts" className="text-light" > <cite title="Source Title">Washington Trails Association, Recreate Responsibly Coalition</cite> </a> </footer>
+        </blockquote>
+      </div>
 
     </div>
     
-
   )
 }
 
